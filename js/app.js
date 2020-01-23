d0b803bc5b593b938615210d5a3b0773f3e538cf
DoaaEssam52 <32943933+DoaaEssam52@users.noreply.github.com>
2020-01-24 01:40:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
 /*****************counter************/
 
-$(document).ready(function() {
-
-    var counters = $(".counting");
-    var countersQuantity = counters.length;
-    var counterr = [];
-  
-    for (i = 0; i < countersQuantity; i++) {
-      counterr[i] = parseInt(counters[i].innerHTML);
-    }
-  
-    var count = function(start, value, id) {
-      var localStart = start;
-      setInterval(function() {
-        if (localStart < value) {
-          localStart++;
-          counters[id].innerHTML = localStart;
-        }
-      }, 40);
-    }
-  
-    for (j = 0; j < countersQuantity; j++) {
-      count(0, counterr[j], j);
-    }
-  });
+$(window).on('scroll', function() { 
+    if ($(window).scrollTop() >= $( 
+      '#counterjs').offset().top + $('#counterjs'). 
+        outerHeight() - window.innerHeight) { 
+        
+            var counters = $(".counting");
+            var countersQuantity = counters.length;
+            var counter = [];
+          
+            for (i = 0; i < countersQuantity; i++) {
+              counter[i] = parseInt(counters[i].innerHTML);
+            }    
+        } 
+        var count = function(start, value, id) {
+            var localStart = start;
+            setInterval(function() {
+              if (localStart < value) {
+                localStart++;
+                counters[id].innerHTML = localStart;
+              }
+            }, 150);
+          }
+        
+          for (j = 0; j < countersQuantity; j++) {
+            count(0, counter[j], j);
+          }
+}); 
 
 /***********************************/
 /*************************************
